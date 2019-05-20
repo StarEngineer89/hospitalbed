698453b3d8b15d5939ea4dca71b9cd1db16e26c1
root <cstardev@gmail.com>
2019-05-20 10:01:30 +0000

    Resources etc files

---
+++ resources/etc/env.php
@@ -0,0 +1,61 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'crypt' => [
+        'key' => '431f3a9b52a57f847e8420cd70a5d65d'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'hospitalbed',
+                'username' => 'taran',
+                'password' => 'taran@789',
+                'active' => '1'
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '7dc_'
+            ],
+            'page_cache' => [
+                'id_prefix' => '7dc_'
+            ]
+        ]
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 0,
+        'block_html' => 0,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 0,
+        'config_webservice' => 1,
+        'translate' => 1,
+        'vertex' => 1
+    ],
+    'install' => [
+        'date' => 'Tue, 07 May 2019 14:39:23 +0000'
+    ]
+];
