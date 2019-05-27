811f25118032977e5ba3f225c111c198693caf86
root <cstardev@gmail.com>
2019-05-27 14:44:38 +0000

    Homepage Slider Changed

---
+++ app/code/Mageplaza/BannerSlider/Ui/Component/Listing/Column/Thumbnail.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Mageplaza
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Mageplaza.com license that is
+ * available through the world-wide-web at this URL:
+ * https://www.mageplaza.com/LICENSE.txt
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Mageplaza
+ * @package     Mageplaza_BannerSlider
+ * @copyright   Copyright (c) Mageplaza (https://www.mageplaza.com/)
+ * @license     https://www.mageplaza.com/LICENSE.txt
+ */
+
+namespace Mageplaza\BannerSlider\Ui\Component\Listing\Column;
+
+use Magento\Framework\DataObject;
+use Magento\Framework\UrlInterface;
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Ui\Component\Listing\Columns\Column;
+use Mageplaza\BannerSlider\Model\Config\Source\Image;
+use Mageplaza\BannerSlider\Model\Config\Source\Type;
+
+/**
+ * Class Thumbnail
+ * @package Mageplaza\BannerSlider\Ui\Component\Listing\Column
+ */
+class Thumbnail extends Column
+{
+    /**
+     * @var Image
+     */
+    protected $imageModel;
+
+    /**
+     * @var UrlInterface
+     */
+    protected $urlBuilder;
+
+    /**
+     * Thumbnail constructor.
+     *
+     * @param ContextInterface $context
+     * @param UiComponentFactory $uiComponentFactory
+     * @param Image $imageModel
+     * @param UrlInterface $urlBuilder
+     * @param array $components
+     * @param array $data
+     */
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        Image $imageModel,
+        UrlInterface $urlBuilder,
+        array $components = [],
+        array $data = []
+    ) {
+        $this->imageModel = $imageModel;
+        $this->urlBuilder = $urlBuilder;
+
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+
+    /**
+     * Prepare Data Source
+     *
+     * @param array $dataSource
+     *
+     * @return array
+     */
+    public function prepareDataSource(array $dataSource)
+    {
+        if (isset($dataSource['data']['items'])) {
+            $fieldName = $this->getData('name');
+            $path = $this->imageModel->getBaseUrl();
+            foreach ($dataSource['data']['items'] as & $item) {
+                $banner = new DataObject($item);
+                if ($item['type'] == Type::IMAGE && $item['image']) {
+                    $item[$fieldName . '_src'] = $path . $item['image'];
+                }
+
+                $item[$fieldName . '_link'] = $this->urlBuilder->getUrl(
+                    'mpbannerslider/banner/edit',
+                    ['banner_id' => $banner->getBannerId(), 'store' => $this->context->getRequestParam('store')]
+                );
+            }
+        }
+
+        return $dataSource;
+    }
+}
